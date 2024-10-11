41a8fd94768d6e354413a8d314725f45d6355612
Adhithya2403 <147901324+Adhithya2403@users.noreply.github.com>
2024-10-11 10:52:13 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,12 +6,20 @@ def chatbot_response(user_input):
         "hi": "Hello! How can I assist you today?",
         "how are you?": "I'm a bot, but I'm doing great! How about you?",
         "bye": "Goodbye! Have a great day!",
+        "Adhithyaa": "HE IS MY MASTER",
+        "Gowri shanker": "HE is the member of THE TEAM",
+        "Kunal": "HE is the member of THE TEAM",
+        "Ashwin ": "HE is the member of THE TEAM",
+        "Team": "The team belongs to JAI SHRIRAM ENGINEERING COLLEGE TIRUPPUR",
+        "How is the guide of the team": "Ms.Gokiavani HOD of cse departnment.JAI SHRIRAM ENGINEERING COLLEGE",
+        "how created you": "Adhithyaa and his team",
+        "how is your master ": "S Adhithyaa kumar 2nd year cse",
     }
     
     return responses.get(user_input.lower(), "I'm sorry, I don't understand that.")
 
 def main():
-    st.title("Simple Chatbot")
+    st.title("Adhithyaa bot")
     
     user_input = st.text_input("You:")
     
